539da793b9406a7660ec2735a923e008b4061a25
cmmerritt <christiane.merritt@gmail.com>
2021-06-13 22:52:47 -0700

    add failing find by id for dollhouses

--- __tests__/dollhouse.test.js
+++ __tests__/dollhouse.test.js
@@ -2,6 +2,20 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Cat from '../lib/models/Cat.js';
+import Dollhouse from '../lib/models/Dollhouse.js';
+
+const fairfield = {
+  name: 'Greenleaf Fairfield', 
+  scale: '1/24', 
+  price: 119
+};
+
+const storybook = {
+  name: 'Greenleaf Storybook Cottage',
+  scale: '1/12',
+  price: 58
+};
 
 describe('dollhouse routes', () => {
   beforeEach(() => {
@@ -11,13 +25,19 @@ describe('dollhouse routes', () => {
   it('creates a dollhouse via POST', async () => {
     const res = await request(app)
       .post('/api/v1/dollhouses')
-      .send({ name: 'Fairfield Greenleaf', scale: '1/24', price: 119 });
+      .send(fairfield);
 
     expect(res.body).toEqual({ 
-      id: '1',
-      name: 'Fairfield Greenleaf',
-      scale: '1/24',
-      price: 119,
+      id: '1', ...fairfield
     });
   });
+
+  it('finds a dollhouse by id via GET', async () => {
+    const dollhouse = await Dollhouse.insert(storybook);
+
+    const res = await request(app).get(`/api/v1/dollhouses/${dollhouse.id}`);
+
+    expect(res.body).toEqual(dollhouse);
+  });
+
 });
