b53cf928b2982bece789885f06023c7495169256
cmmerritt <christiane.merritt@gmail.com>
2021-06-14 11:39:08 -0700

    find game by id test fails

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Game from '../lib/models/Game.js';
 
 const portal = {
   title: 'Portal',
@@ -20,5 +21,13 @@ describe('game routes', () => {
       .send(portal);
 
     expect(res.body).toEqual({ 'id': '1', ...portal });
+  });
+
+  it('finds a game by id via GET', async () => {
+    const game = await Game.insert(portal);
+
+    const res = await request(app).get(`/api/v1/games/${game.id}`);
+
+    expect(res.body).toEqual(game);
   });
 });
