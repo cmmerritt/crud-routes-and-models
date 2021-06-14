b9dceab37e140d478147e3635ca2c0c5a710aabb
cmmerritt <christiane.merritt@gmail.com>
2021-06-13 22:31:44 -0700

    dollhouse put route passes

--- __tests__/dollhouse.test.js
+++ __tests__/dollhouse.test.js
@@ -2,7 +2,6 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Dollhouse from '../lib/models/Dollhouse.js';
 
 describe('dollhouse routes', () => {
   beforeEach(() => {
@@ -11,7 +10,7 @@ describe('dollhouse routes', () => {
 
   it('creates a dollhouse via POST', async () => {
     const res = await request(app)
-      .post('api/v1/dollhouses')
+      .post('/api/v1/dollhouses')
       .send({ name: 'Fairfield Greenleaf', scale: '1/24', price: 119 });
 
     expect(res.body).toEqual({ 
